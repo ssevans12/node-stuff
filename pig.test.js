4bdbeabc51b7af4e42165429943078b9b42c6f2c
Sara Evans <ssevans@umich.edu>
2023-03-18 19:02:28 -0400

    all tests have passed

--- pig.test.js
+++ pig.test.js
@@ -2,7 +2,17 @@ const pig = require("./pig")
 describe("pig", () => {
     test("word starts with vowel", () => {
         expect(pig.translate("apple")).toEqual("appleway");
-        
-    })
-    
+    });
+    test("word starts with consonant", () => {
+        expect(pig.translate("consonant")).toEqual("onsonantcay");
+    });
+    test("phrase starts with vowel", () => {
+        expect(pig.translate("apple orange")).toEqual("appleway orangeway");
+    });
+    test("phrase starts with consonant", () => {
+        expect(pig.translate("hungry hippo")).toEqual("ungryhay ippohay");
+    });
+    test("mixed phrase", () => {
+        expect(pig.translate("sara evans is great")).toEqual("arasay evansway isway eatgray");
+    });
 });
