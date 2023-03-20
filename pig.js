cfca2860fc8842ef489a3b86f3fcc4ceccad0a6d
Sara Evans <ssevans@umich.edu>
2023-03-20 17:57:59 -0400

    added more comments in my code

--- pig.js
+++ pig.js
@@ -10,6 +10,7 @@ function translate(phrase){
         } else {
             let firstVowelIndex = findFirstVowelIndex(word, vowels);
             let firstConsonants = word.substring(0, firstVowelIndex);
+            // This will return the rest of the word between firstvowelindex, and end of word
             word = word.substring(firstVowelIndex);
             phrase += " " + word + firstConsonants + "ay";
         }
@@ -19,10 +20,13 @@ function translate(phrase){
     return phrase.trim();
 }
 
+// this function is finding the index (where vowel falls in word) of the first vowel
 function findFirstVowelIndex(word, vowels){
     let lowestIndex = 1000;
     vowels.forEach(v => {
         let currentIndex = word.indexOf(v);
+        // if the current index is less than the lowest index 
+        // example "treat"= a is 3, e is 2- since 3 is less than 1000 (lowest index), it will set it to three initially, but e is 2, so will replace it
         if(currentIndex < lowestIndex && currentIndex >= 0) {
             lowestIndex = currentIndex;
         }
